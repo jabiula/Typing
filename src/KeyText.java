ea5ca79b2f65426ff8008a20747ee4ce2e4621f0
jabiula <mohammad.jabiula@gmail.com>
2018-02-06 17:29:02 +0545

    documentation is added to language.java and keytext.java

--- src/KeyText.java
+++ src/KeyText.java
@@ -5,9 +5,17 @@
  * {@link http://www.itspl.com.np/}
  */
 public class KeyText {
-String upper,lower;
-int size;
+String upper,lower;//Upper stores the upper character and lower stores the lower character
+int size; //stores the size of the key
 
+
+
+/**
+ * 
+ * @param upper Letter to show when shift key is pressed.
+ * @param lower Letter to show when shift key is not pressed.
+ * @param size Width of the key
+ */
 public KeyText(String upper, String lower,int size) {
 	super();
 	this.upper = upper;
@@ -15,13 +23,27 @@ public KeyText(String upper, String lower,int size) {
 	this.size = size;
 }
 
+
+/**
+ * This getter will return upper case of the key
+ * @return upper case character
+ */
 public String getUpper() {
 	return upper;
 }
-
+/**
+ * This getter will return lower case of the key
+ * @return lower case character
+ */
 public String getLower() {
 	return lower;
 }
+
+/**
+ * This getter will return the width of the key in the keyboard.
+ * @return the width of the key
+ */
+
 public int getSize()
 {
 	return size;
--- src/Language.java
+++ src/Language.java
@@ -3,6 +3,9 @@
  * @version 1.0
  * @since 2018
  * {@link http://www.itspl.com.np/}
+ * 
+ * Supported language by Keyboard
+ *
  */
 public enum Language {
 English,Nepali
