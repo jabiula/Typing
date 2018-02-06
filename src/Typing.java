098a8d5476708bfb55d6e7d61c8142a6d93bbcd7
jabiula <mohammad.jabiula@gmail.com>
2018-02-06 16:36:11 +0545

    i have changed the title

--- src/Typing.java
+++ src/Typing.java
@@ -6,7 +6,7 @@ public class Typing {
 		
 		Keyboard kb = new Keyboard(Language.English);
 		
-		JFrame window = new JFrame("English Keyboard by atish key");
+		JFrame window = new JFrame("English Keyboard by atishjava");
 		window.add(kb);
 		window.setSize(728,300);
 		window.setDefaultCloseOperation(3);
