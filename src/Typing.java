9ce80b845fea92c07a4861b519c5ce6a4756c933
jeevanjava <jeevanstha143@gmail.com>
2018-02-06 15:15:06 +0545

    Modified version by jeevan in title

--- src/Typing.java
+++ src/Typing.java
@@ -6,7 +6,7 @@ public class Typing {
 		
 		Keyboard kb = new Keyboard(Language.English);
 		
-		JFrame window = new JFrame("English Keyboard");
+		JFrame window = new JFrame("English Keyboard by Jeevan");
 		window.add(kb);
 		window.setSize(728,300);
 		window.setDefaultCloseOperation(3);
